e5a255b8e66472fa396ebe1478864afd8303bd48
Nicolas Oidtmann <n.c.oidtmann@student.tudelft.nl>
2024-06-20 18:00:57 +0200

    working session end

--- sa_drive/Motor_code/single_motor_2_UNOs/single_motor_2_UNOs_Master/single_motor_2_UNOs_Master.ino
+++ sa_drive/Motor_code/single_motor_2_UNOs/single_motor_2_UNOs_Master/single_motor_2_UNOs_Master.ino
@@ -1,37 +1,73 @@
 #include <SoftwareSerial.h>
-#include <Stepper.h>
+#include <TimerOne.h>
 
-// Master code
-// Master monitors heartbeat of Slave and takes over control if Slave stops working
+// PIN allocations for Software Serial
+int SUART_IN = 2;
+int SUART_OUT = 3;
 
-SoftwareSerial SUART(2, 3);  // RX, TX for Software Serial
+SoftwareSerial SUART(SUART_IN, SUART_OUT);  // RX, TX for Software Serial
 
-unsigned long lastHeartbeatTime = 0;
-const unsigned long heartbeatTimeout = 2000;  // Timeout period in milliseconds
+volatile bool receivedPing = false;
+volatile bool waitingForPing = false;
+volatile unsigned long lastPingTime = 0;
+volatile bool SlaveOperational = true;
+
+int RequestPingTimer = 1; // seconds
 
 void setup() {
   Serial.begin(9600);  // Start Serial communication for debugging
   SUART.begin(9600);   // Initialize software serial communication
+  pinMode(SUART_IN, INPUT);
+  pinMode(SUART_OUT, OUTPUT);
+
+  // Initialize Timer1 to trigger every second
+  Timer1.initialize(RequestPingTimer*1000000);  // Set timer for 1 second (1,000,000 microseconds)
+  Timer1.attachInterrupt(requestPing);  // Attach the requestPing function to the timer interrupt
 }
 
 void loop() {
-  // Check for heartbeat signal
+  // printSlaveStatus(SlaveOperational);
 
-  if (SUART.available()) {
-    String message = SUART.readStringUntil('\n');
-    Serial.print("Received message: ");
-    Serial.println(message);
-    message.trim();
-    if (message == "heartbeat") {
-      lastHeartbeatTime = millis();  // Reset the heartbeat timer
-      Serial.println("Master: Received heartbeat");
-    }
+  if (receivedPing) {
+    Serial.println("Master: Received ping from slave");
+    receivedPing = false;  // Reset flag
+    waitingForPing = false;  // Reset waiting flag
   }
 
-  if (millis() - lastHeartbeatTime > heartbeatTimeout) {
-    // Master has stopped working, print a message
-    Serial.println("Master: Slave not working, taking over.");
-    // Add logic to take over control here
-    lastHeartbeatTime = millis();  // Reset the timer to avoid continuous printing
+  // Check if we are waiting for a ping and if the timeout has occurred
+  if (waitingForPing && (millis() - lastPingTime > 2000)) {
+    Serial.println("Master: Slave not responding, declaring slave as not working");
+    SlaveOperational = false;
+    receivedPing = false;
+    waitingForPing = false;  // Reset waiting flag
+  }
+
+  // Check for response from slave
+  if (SUART.available()) {
+    String response = SUART.readStringUntil('\n');
+    response.trim();
+    if (response == "ping") {
+      receivedPing = true;
+    }
+  }
+  delay(100);
+}
+
+// Timer interrupt function to request ping
+void requestPing() {
+  SUART.println("requestPing");
+  Serial.println("Master: Sent ping request to slave");
+  if (waitingForPing == false){
+    lastPingTime = millis();  // Record the time of the request
+  }
+  waitingForPing = true;  // Set waiting flag
+}
+
+void printSlaveStatus(bool status) {
+  if (status){
+    Serial.println("Slave is operational");
+  }
+  else {
+    Serial.println("Slave NOT WORKINGGG!!!");
   }
 }
--- sa_drive/Motor_code/single_motor_2_UNOs/single_motor_2_UNOs_Slave/single_motor_2_UNOs_Slave.ino
+++ sa_drive/Motor_code/single_motor_2_UNOs/single_motor_2_UNOs_Slave/single_motor_2_UNOs_Slave.ino
@@ -1,21 +1,85 @@
 #include <Stepper.h>
 #include <SoftwareSerial.h>
+#include <TimerOne.h>
 
 // Slave code
 // Slave sends heartbeat signal to Master every second
 // If Slave stops sending heartbeat, Master takes over control
 
-SoftwareSerial SUART(2, 3);  // RX, TX for Software Serial
+int stepsPerRevolution = 2048;
+Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
+
+
+// PIN allocations
+int SUART_IN = 2;
+int SUART_OUT = 3;
+
+int angleIn = 0;
+
+SoftwareSerial SUART(SUART_IN, SUART_OUT);  // RX, TX for Software Serial
+
+
 
 void setup() {
   Serial.begin(9600);  // Start Serial communication for debugging
   SUART.begin(9600);   // Initialize software serial communication
+  pinMode(SUART_IN, INPUT);
+  pinMode(SUART_OUT, OUTPUT);
+
+  // Initialize Timer1 to trigger every 100ms for checking SUART
+  Timer1.initialize(100000);  // Set timer for 100ms (100,000 microseconds)
+  Timer1.attachInterrupt(checkSUART);  // Attach the checkSUART function to the timer interrupt
 }
 
 void loop() {
-  // Send heartbeat signal to the slave
-  SUART.println("heartbeat");
-  Serial.println("Slave: Sent heartbeat");
+  // nothing to do here...
+}
+
+// This function is called whenever data is received on the serial port
+void serialEvent() {
+  while (Serial.available()) {
+    angleIn = Serial.parseInt();
+    moveSM_to_angle(angleIn);
+  }
+}
+
+// Timer interrupt function to check SUART
+void checkSUART() {
+  if (SUART.available()) {
+    String request = SUART.readStringUntil('\n');
+    request.trim();
+    if (request == "requestPing") {
+      sendPing();
+    }
+  }
+}
+
+// Function to send ping response
+void sendPing() {
+  SUART.println("ping");
+  Serial.println("Slave: Sent ping response to master");
+}
+
+void moveSM_to_angle(long angle) {
+  long steps = map(angle, 0, 360, 0, stepsPerRevolution);
+
+  if (angle >= 0) {
+    myStepper.setSpeed(10);
+  } else {
+    myStepper.setSpeed(-10);
+  }
+
+  for (long i = 0; i < abs(steps); ++i) {
+    myStepper.step(steps > 0 ? 1 : -1);
+    // if (!takeDigitalReadings()) {
+    //   Serial.println("Motor not working");
+    //   return;
+    // }
+    delay(10);  // Short delay to allow the motor to step smoothly
+  }
+  Serial.print("Motor moved to angle: ");
+  Serial.println(angle);
   
-  delay(1000);  // Send heartbeat every second
+  Serial.println(steps);
+  delay(1000);
 }
